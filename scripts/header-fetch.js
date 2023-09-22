a24e1b022a346fd4e866b382faa7cd53c708641b
elite784 <73318856+epc9020@users.noreply.github.com>
2023-09-22 12:57:03 -0400

    Update header-fetch.js

--- scripts/header-fetch.js
+++ scripts/header-fetch.js
@@ -1,4 +1,4 @@
-      (await function () {
+      (async function () {
         const headerHTMLRequest = await fetch('/pagefile/header.html');
         const headerHTML = await headerHTMLRequest.text();
         document.querySelector('.header-container').innerHTML = headerHTML;
