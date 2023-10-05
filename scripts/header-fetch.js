4b7a932219df86d1ca1631b48cb2ba7e81f27dcb
elite784 <73318856+epc9020@users.noreply.github.com>
2023-10-05 10:54:40 -0400

    yyy

--- scripts/header-fetch.js
+++ scripts/header-fetch.js
@@ -1,5 +1,15 @@
-      (async function () {
-        const headerHTMLRequest = await fetch('/pagefile/header.html');
-        const headerHTML = await headerHTMLRequest.text();
-        document.querySelector('.header-container').innerHTML = headerHTML;
-      })();
+(async function () {
+  const headerHTMLRequest = await fetch('/pagefile/header.html');
+  const headerHTML = await headerHTMLRequest.text();
+  document.querySelector('.header-container').innerHTML = headerHTML;
+})();
+(async function () {
+  const headerHTMLRequest = await fetch('/textfile/updates.html');
+  const headerHTML = await headerHTMLRequest.text();
+  document.querySelector('.updatestext').innerHTML = headerHTML;
+})();
+(async function () {
+  const headerHTMLRequest = await fetch('/pagefile/header.html');
+  const headerHTML = await headerHTMLRequest.text();
+  document.querySelector('.header-container').innerHTML = headerHTML;
+})();
