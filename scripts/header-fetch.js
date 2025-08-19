dd3b0f6c1a005b864f72a52efa859f729e43d743
epc9020 <epp9020@outlook.com>
2025-08-19 07:48:34 -0400

    MAJOR SITE OVERHAUL

--- scripts/header-fetch.js
+++ scripts/header-fetch.js
@@ -1,19 +1,21 @@
+//TODO: rehaul entire script to choose what file to get from what folder to fetch depending on the set language data
 
-function desktopGet() {
-  (async function () {
-    const headerHTMLRequest = await fetch('/pagefile/header.html');
+
+
+async function changeLanguage(lang) {
+    await setLanguagePreference(lang);
+    
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+    toggleStylesheet(lang); // switch stylesheets
+}
+
+(async function () {
+    const headerHTMLRequest = await fetch(`/fetch/header.html`);
     const headerHTML = await headerHTMLRequest.text();
     document.querySelector('.header-container').innerHTML = headerHTML;
   })();
-}
 
-function mobileGet() {
-  (async function () {
-    const headerHTMLRequest = await fetch('/pagefile/mobileheader.html');
-    const headerHTML = await headerHTMLRequest.text();
-    document.querySelector('.updatestext').innerHTML = headerHTML;
-  })();
-}
 
 
 
